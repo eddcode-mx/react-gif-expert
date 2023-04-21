ff6baf3dd0a4ae2003613a83d2f517438ce42aaa
eddcode-mx <eddcodemx@gmail.com>
2023-04-20 19:23:44 -0600

    Primer commit

---
+++ src/GifExpertApp.jsx
@@ -0,0 +1,27 @@
+import { useState } from 'react';
+import { AddCategory, GifGrid } from './components';
+
+export const GifExpertApp = () => {
+  const [categories, setCategories] = useState(['Spiderman']);
+
+  const onAddCategory = (onNewCategory) => {
+    if (categories.includes(onNewCategory)) return;
+
+    setCategories([onNewCategory, ...categories]);
+  };
+
+  return (
+    <>
+      <h1>GifExpertApp</h1>
+
+      <AddCategory
+        // setCategories={setCategories}
+        onNewCategory={(value) => onAddCategory(value)}
+      />
+
+      {categories.map((category) => (
+        <GifGrid key={category} category={category} />
+      ))}
+    </>
+  );
+};
